f67bada1e4ccdcd9397060ca7fac89895313f7e1
eternallycyf <969475322@qq.com>
2023-12-18 21:23:48 +0800

    ✨ feat: add mdx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ThemeConfig from '@/theme/themeConfig';
 import locale from 'antd/locale/zh_CN';
 import Demo from '@/components/demo/index';
 import Link from 'next/link';
+import HelloWorld from './hello-world/page.mdx';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           componentSize="small"
           theme={ThemeConfig}
         >
+          <HelloWorld />
           <Link href="/proxy">proxy</Link>
           <Demo />
         </ConfigProvider>
